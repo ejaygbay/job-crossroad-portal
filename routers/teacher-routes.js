cbd62d4a573df25f91b410270b5d1b9cb35e83ad
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-06 19:49:18 +0000

    teacher routes linked to views.

--- routers/teacher-routes.js
+++ routers/teacher-routes.js
@@ -4,4 +4,12 @@ routers.get('/teachers', (req, res) => {
     res.render('teachers/all-teachers');
 })
 
+routers.get('/teachers/create', (req, res) => {
+    res.render('teachers/create-teacher');
+})
+
+routers.get('/teachers/edit', (req, res) => {
+    res.render('teachers/edit-teacher');
+})
+
 module.exports = routers;
