67f14754ee9f40291f7bee286063ad81ab2be321
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-06 18:17:27 +0000

    views tamplate file created to be used for creating new pages.

--- routers/subject-routes.js
+++ routers/subject-routes.js
@@ -4,4 +4,9 @@ routers.get('/subjects', (req, res) => {
     res.render('subjects/all-subjects');
 })
 
+routers.get('/subjects/create', (req, res) => {
+    res.render('views-template/views-template');
+    // res.render('subjects/all-subjects');
+})
+
 module.exports = routers;
