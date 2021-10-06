ae0881c0a1912f286be6de929872622125814c36
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-06 19:11:25 +0000

    classes routes linked to views.

--- routers/class-routes.js
+++ routers/class-routes.js
@@ -1,7 +1,15 @@
 const routers = require('express').Router();
 
-routers.get('/teachers', (req, res) => {
-    res.render('teachers/all-teachers');
+routers.get('/classes', (req, res) => {
+    res.render('classes/all-classes');
+})
+
+routers.get('/classes/create', (req, res) => {
+    res.render('classes/create-class');
+})
+
+routers.get('/classes/sponsor', (req, res) => {
+    res.render('classes/assign-sponsor');
 })
 
 module.exports = routers;
