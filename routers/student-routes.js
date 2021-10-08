8f86387e656390f45b543f09c89472db311f9c07
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-08 05:00:36 +0000

    Student routes linked to views.

--- routers/student-routes.js
+++ routers/student-routes.js
@@ -1,7 +1,15 @@
 const routers = require('express').Router();
 
-routers.get('/Student', (req, res) => {
-    res.send("Student");
+routers.get('/students', (req, res) => {
+    res.render('students/all-students');
+})
+
+routers.get('/students/create', (req, res) => {
+    res.render('students/create-student');
+})
+
+routers.get('/students/edit', (req, res) => {
+    res.render('students/edit-student');
 })
 
 module.exports = routers;
