17d25aa116a4e36ac9fa3a1047850ce694ac57ab
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-30 09:47:54 +0000

    Dynamic port added to project.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const authRoutes = require('./routers/auth');
 const dashboardRoutes = require('./routers/dashboard-routes');
 const teacherRoutes = require('./routers/teacher-routes');
