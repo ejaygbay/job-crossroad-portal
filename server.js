cd1716818db285beefd086774ccb772620a8c3d7
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-30 09:56:13 +0000

    package.json file re-inited.

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const PORT = process.env.PORT || 3000;
 const authRoutes = require('./routers/auth');
 const dashboardRoutes = require('./routers/dashboard-routes');
 const teacherRoutes = require('./routers/teacher-routes');
-const studentRoutes = require('./routers/student-routes');
-const reportRoutes = require('./routers/report-routes');
-const parentRoutes = require('./routers/parent-routes');
-const classRoutes = require('./routers/class-routes');
 const subjectRoutes = require('./routers/subject-routes');
 
 
@@ -18,10 +14,6 @@ app.use(express.static(__dirname, +'/public'));
 app.use(authRoutes);
 app.use(dashboardRoutes);
 app.use(teacherRoutes);
-app.use(studentRoutes);
-app.use(reportRoutes);
-app.use(parentRoutes);
-app.use(classRoutes);
 app.use(subjectRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
