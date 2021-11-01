b70f07f351bf37ac0b16553e0cd5c8e9bca4a668
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-11-01 07:12:07 +0000

    mm-active class added to active menu.

--- routers/job-routes.js
+++ routers/job-routes.js
@@ -1,15 +1,15 @@
 const routers = require('express').Router();
 
 routers.get('/jobs/personal', (req, res) => {
-    res.render('jobs/personal-jobs');
+    res.render('jobs/personal-jobs', { page: 'personal-jobs' });
 })
 
 routers.get('/jobs/create', (req, res) => {
-    res.render('jobs/create-job');
+    res.render('jobs/create-job', { page: 'create-job' });
 })
 
 routers.get('/jobs', (req, res) => {
-    res.render('jobs/all-jobs');
+    res.render('jobs/all-jobs', { page: 'all-jobs' });
 })
 
 module.exports = routers;
