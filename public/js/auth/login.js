0ad40f09b69730d7000b47924286fcbb34c2cfb1
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-11-06 22:20:54 +0000

    Function added to login.js page.

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -1,53 +1,47 @@
-document.querySelector('#create-account-btn').addEventListener('click', (e) => {
-    let first_name = document.getElementById('first-name').value.trim();
-    let middle_name = document.getElementById('middle-name').value.trim();
-    let last_name = document.getElementById('last-name').value.trim();
-    let contact = document.getElementById('contact').value.trim();
+document.querySelector('#login-btn').addEventListener('click', (e) => {
     let email = document.getElementById('email').value.trim();
     let password = document.getElementById('password').value.trim();
 
-    let account_details = {
-        first_name: first_name,
-        last_name: last_name,
-        contact: contact,
+    let login_details = {
         email: email,
         password: password
     }
 
-    if (!validateInputs(account_details)) {
-        account_details.middle_name = middle_name;
+    if (!validateInputs(login_details)) {
 
-        makeRequest(account_details, result => {
-            if (result.code === 0) {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Account created successfully',
-                    showConfirmButton: false,
-                    timer: 2000
-                })
-            } else if (result.code === 2) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Email already in use',
-                    showConfirmButton: false,
-                    timer: 2000
-                })
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'An error has occurred',
-                    showConfirmButton: false,
-                    timer: 2000
-                })
-            }
-        })
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Make sure all the required fields are filled in',
-            showConfirmButton: false,
-            timer: 2500
-        })
+        makeRequest(login_details, result => {
+                console.log(result);
+
+                //         if (result.code === 0) {
+                //             Swal.fire({
+                //                 icon: 'success',
+                //                 title: 'Account created successfully',
+                //                 showConfirmButton: false,
+                //                 timer: 2000
+                //             })
+                //         } else if (result.code === 2) {
+                //             Swal.fire({
+                //                 icon: 'error',
+                //                 title: 'Email already in use',
+                //                 showConfirmButton: false,
+                //                 timer: 2000
+                //             })
+                //         } else {
+                //             Swal.fire({
+                //                 icon: 'error',
+                //                 title: 'An error has occurred',
+                //                 showConfirmButton: false,
+                //                 timer: 2000
+                //             })
+                //         }
+            })
+            // } else {
+            //     Swal.fire({
+            //         icon: 'error',
+            //         title: 'Make sure all the required fields are filled in',
+            //         showConfirmButton: false,
+            //         timer: 2500
+            //     })
     }
 })
 
